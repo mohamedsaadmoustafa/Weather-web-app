6fe6840a90464df31a02c242225fb1a3c5e10e1d
Mohamed Saad <mohamedsaadmoustafa@gmail.com>
2023-01-25 23:26:38 +0200

    Update forecastdata.js

--- modules/forecastdata.js
+++ modules/forecastdata.js
@@ -1,21 +1,22 @@
-require('dotenv').config();
-const request = require('request');
+const axios = require('axios');
+
 // access hidden keys in the .env file.
-const dotenv = require('dotenv');
+require('dotenv').config()
 
 const API_KEY = process.env.API_KEY;
 var BASE_URL = 'https://api.openweathermap.org/data/2.5/forecast'
 
 module.exports = (cityName, callback) => {
   var url = BASE_URL+`?q=${cityName}&appid=${API_KEY}&units=metric`
-  request({ url: url, json: true }, function (error, response) {
-    if (error) {
-      console.log('Unable to connect to Forecast API', undefined);
-    }
-    else {
-      console.log("Start getting weather data!");
-      data = response.body.list
-      callback(undefined, data)
-    }
+
+  axios
+  .get(url)
+  .then(response => {
+    console.log("Start getting weather data!");
+    data = response.data.list
+    callback(undefined, data)
   })
+  .catch(error => {
+    console.log('Unable to connect to Forecast API', undefined);
+  });
 }
